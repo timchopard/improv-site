dee3b0f4c9db9ba1309f5f7d45fc31926b6a3cb7
tim <timothyjchandler@gmail.com>
2024-08-20 16:38:47 +0200

    tweaking static

--- improv/improv/settings.py
+++ improv/improv/settings.py
@@ -105,7 +105,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-STATICFILES_DIRS = ('static', )
+# STATICFILES_DIRS = ('static', )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
